95b1c2cd8718124085e5850bc86b9739345e5d14
Ionut Cristian Ciuperca <ionut.ciuperca@gmail.com>
2012-07-19 16:16:21 +0100

    modified index.php to take advantage of the utility classes

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <?php
-echo "Hello";
+
+// define system constants
+
+define ('APPFOLDER', 'app');
+define ('SYSTEMFOLDER', 'lib');
+define ('CONTROLLERS', APPFOLDER . '/controllers');
+define ('MODELS', APPFOLDER . '/models');
+define ('TEMPLATES', APPFOLDER . '/templates');
+define ('CONFIGFOLDER', APPFOLDER . '/config');
+define ('CONFIGFILE', CONFIGFOLDER . '/config.ini');
+
+
+
+require_once SYSTEMFOLDER . '/router.php';
+require_once SYSTEMFOLDER . '/dispatcher.php';
+
+
+$uri = $_SERVER['REQUEST_URI'];
+$dispatcher = new Dispatcher($uri);
+$dispatcher->dispatch();
