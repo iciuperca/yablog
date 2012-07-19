5d18da78a74cf81d092847cab0c9452de2bf7d20
Ionut Cristian Ciuperca <ionut.ciuperca@gmail.com>
2012-07-19 16:33:11 +0100

    added documentation for the posts model

--- app/models/post_model.php
+++ app/models/post_model.php
@@ -8,11 +8,20 @@ require_once SYSTEMFOLDER . '/model.php';
 
 class Post_Model extends Model {
 
+    /**
+     * Return an array of all the posts matching the query
+     * @return array The result of teh query
+     */
     function get_all_posts() {
         $posts = $this->getAssocArray('SELECT post_id, title FROM posts');
         return $posts;
     }
 
+    /**
+     * Search for a post based on the slug
+     * @param string $slug The slug of the post
+     * @return array
+     */
     public function get_by_slug($slug) {
         $sql = "SELECT * FROM `posts`
             WHERE `slug`=?
