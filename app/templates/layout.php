0ba6a40466abda36e217c66e27aa6b26a33704c9
Ionut Cristian Ciuperca <ionut.ciuperca@gmail.com>
2012-07-21 11:06:26 +0100

    Changed the layout to take advantage of tweeter bootstrap

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -3,12 +3,25 @@
     <head>
         <meta charset="utf-8" />
         <title><?php echo $title ?></title>
-
-        <link rel="stylesheet" href="css/main.css" type="text/css" />
+        <link href="/static/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="/static/css/main.css" type="text/css" />
 
     </head>
 
-    <body id="index" class="home">
-        <?php echo $_content ?>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="span12">
+                    <h1>YaBlog</h1>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="span4">Navigation</div>
+                <div class="span8">
+                    <?php echo $_content ?>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
