756de0ea83c646b9b3a2d98efcb79ba614cdd8f6
Ionut Cristian Ciuperca <ionut.ciuperca@gmail.com>
2012-07-24 11:23:09 +0100

    Added session initialization and check if there are any uri arguments (and discard them if there are)

--- index.php
+++ index.php
@@ -14,8 +14,18 @@ define ('CONFIGFILE', CONFIGFOLDER . '/config.ini');
 
 require_once SYSTEMFOLDER . '/router.php';
 require_once SYSTEMFOLDER . '/dispatcher.php';
+require_once SYSTEMFOLDER . '/session.php';
 
+$session = Session::getInstance();
+$session->start();
 
-$uri = $_SERVER['REQUEST_URI'];
+$uri_segments  = explode('?', $_SERVER['REQUEST_URI']);
+if(count($uri_segments) > 1) {
+    $uri = $uri_segments[0];
+} else {
+    $uri = $_SERVER['REQUEST_URI'];
+}
 $dispatcher = new Dispatcher($uri);
 $dispatcher->dispatch();
+
+$session->close();
