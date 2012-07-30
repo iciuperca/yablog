2712ed1227c47c7feadcd9df241df61aafff1776
Ionut Cristian Ciuperca <ionut.ciuperca@gmail.com>
2012-07-30 11:34:03 +0100

    Created the user login view

---
+++ app/templates/users/login.php
@@ -0,0 +1,21 @@
+<?php $_extends = 'app/templates/layout.php' ?>
+
+<h1><?php echo $title ?></h1>
+
+<?php if (isset($errors)): ?>
+    <div class="errors">
+        <?php echo $errors ?>
+    </div>
+ <?php endif; ?>
+
+
+<div id="login-form">
+    <form action="" method="post">
+    <label for="username">Username:</label>
+    <input type="text" required="required" name="username" id="username" />
+    <label for="password">Password:</label>
+    <input type="password" required="required" name="password" id="password" />
+
+    <input type="submit" name="login-submit" value="Submit" />
+    </form>
+</div>
